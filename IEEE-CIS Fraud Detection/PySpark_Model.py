00dd0b4bbe57636d378ae8c8b26dceabbeb2fa46
AiIsBetter <309236328@qq.com>
2019-12-30 21:42:07 +0800

    总算有时间又写了点

--- IEEE-CIS Fraud Detection/PySpark_Model.py
+++ IEEE-CIS Fraud Detection/PySpark_Model.py
@@ -8,10 +8,7 @@ Code: https://github.com/AiIsBetter
 # os.environ['PYSPARK_PYTHON'] = '/home/aigege/.conda/envs/py3-conda/bin/python3.6'
 from pyspark import SparkContext,SparkConf
 from pyspark.sql import SparkSession
-import pyspark.sql.functions  as f
-import matplotlib.pyplot as plt
-import pandas as pd
-from pyspark.sql import Row
+import numpy as np
 import gc
 import time
 from contextlib import contextmanager
@@ -73,25 +70,41 @@ def timer(title):
 
 def main(path = None,run_mode = 'standalone'):
     if(run_mode == 'standalone'):
-        spark = SparkSession.builder.master('spark://aigege-OMEN-by-HP-Laptop-15-dc0xxx:7077').appName('train').getOrCreate()
+        #问题1：刚开始用Pyspark,发现伪分布式下面，默认读取所有core，比如我的电脑是12，然后executor直接通过spark的配置文件spark-env.sh无法修改，总是只有一个executor，里面的
+        # SPARK_WORKER_MEMORY, to set how much total memory workers have to give executors，设置了以后是整个worker的memory，executor默认1024mb，所以下面的.config('spark.executor.memory',
+        # '5g')可以设置较大的executor内存,其他的参数类似config('spark.num.executors', '2').config('spark.executor.cores', '6')都可以使用，按需选择即可
+        spark = SparkSession.builder.master('spark://aigege-OMEN-by-HP-Laptop-15-dc0xxx:7077').appName('train').config('spark.executor.memory', '5g').\
+            getOrCreate()
     elif(run_mode == 'online'):
         spark = SparkSession.builder.appName(
             'test').getOrCreate()
     with timer("Load data finish:"):
         # LOAD TRAIN
-        X_train = spark.read.csv(path=path+'train_transaction.csv', schema=None, sep=',', header=True).select(cols + ['isFraud'])
-        train_id = spark.read.csv(path=path+'train_identity.csv', schema=None, sep=',', header=True)
-        train_id.show()
+        # 问题2：直接用read.csv的默认参数读取hdfs上面的csv，会导致所有列的类型被读取为string，需要设置inferSchema=True，来自动推测列类型，达到pandas的read_csv的效果
+        X_train = spark.read.csv(path=path+'train_transaction.csv', schema=None, sep=',', header=True,inferSchema=True).select(cols + ['isFraud'])
+        train_id = spark.read.csv(path=path+'train_identity.csv', schema=None, sep=',', header=True,inferSchema=True)
+        print(X_train.dtypes)
         X_train = X_train.join(train_id, on='TransactionID', how='left')
         print((X_train.count(), len(X_train.columns)))
         # LOAD TEST
-        X_test = spark.read.csv(path=path+'test_transaction.csv', schema=None, sep=',', header=True).select(cols)
-        test_id = spark.read.csv(path=path+'test_identity.csv', schema=None, sep=',', header=True)
+        X_test = spark.read.csv(path=path+'test_transaction.csv', schema=None, sep=',', header=True,inferSchema=True).select(cols)
+        test_id = spark.read.csv(path=path+'test_identity.csv', schema=None, sep=',', header=True,inferSchema=True)
         X_test = X_test.join(test_id, on='TransactionID', how='left')
+        print(X_test.dtypes)
         print((X_test.count(), len(X_test.columns)))
-
-
-
+        # TARGET
+        y_train = X_train[['isFraud']]
+        X_train = X_train.drop('isFraud')
+        X_train.show()
+        print(X_train.dtypes)
+    with timer("Data Preprocess"):
+        # NORMALIZE D COLUMNS
+        for i in range(1, 16):
+            if i in [1, 2, 3, 5, 9]: continue
+            # 问题3：Pysparkb不能除以numpy.float32,会报错，上网查了下，说是两种type还是不太一样。所以这里直接乘以1.0，用python的float
+            X_train= X_train.withColumn('D' + str(i),X_train['D' + str(i)] - X_train['TransactionDT']*1.0/ 24 * 60 * 60)
+            X_test = X_test.withColumn('D' + str(i),X_test['D' + str(i)] - X_test['TransactionDT'] * 1.0 / 24 * 60 * 60)
+        X_train.show()
 
 if __name__ == "__main__":
     path = "hdfs://localhost:9000/user/kaggle_fraud_detection/data/"
